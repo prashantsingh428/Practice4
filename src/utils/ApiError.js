c93abbf39d63566b2d9462f45bd639e713e75914
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2025-10-19 15:21:03 +0530

    adding and updating files

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,14 +2,14 @@ class ApiError extends Error {
     construtor(
         statusCode,
         message= "Something went wrong",
-        error= [],
+        errors= [],
         stack = ""
     ){
         super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.data = null;
-        this.error = this.error;
+        this.errors = this.errors;
         this.stack = stack;
         this.success = false;
         
